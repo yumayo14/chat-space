dae45f118803e4f39f2ff7191c1e3ea94ebb787f
コミットしました <yumayo14@gmail.com>
2017-12-07 17:24:06 +0900

    編集時に[ name ]カラムの情報を、受け取れるようにするための設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:update, keys: [:name])
   end
 end
