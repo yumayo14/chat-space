ff604841011d797a99b3c1e33d50d784df095c1b
コミットしました <yumayo14@gmail.com>
2017-12-08 17:19:04 +0900

    フラッシュ機能の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(account_params)
-      redirect_to controller: :messages, action: :index
+      redirect_to root_path, notice: :"ユーザー情報の更新に成功しました"
     else
-      render :edit
+      render :edit, alert: :"ユーザー情報の更新に失敗しました"
     end
   end
 
