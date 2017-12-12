a25aae77d8eea8b06ae3a550b838877e11f30a20
コミットしました <yumayo14@gmail.com>
2017-12-12 15:59:24 +0900

    画像の投稿ができなくなったので、その修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -14,7 +14,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_limit: [300, 200, "Center"]
+  process resize_to_limit: [300, 200]
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
