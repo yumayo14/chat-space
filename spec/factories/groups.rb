80a4986f35aa9316257706c7277ddb1b67475cb9
コミットしました <yumayo14@gmail.com>
2017-12-15 08:22:31 +0900

    Fakerを用いて、ランダムに情報を生成できるように設定

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
-
   factory :group do
-    name "やっほー"
+    name      Faker::Name.name
   end
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
+
+  pass = Faker::Internet.password
+
   factory :user do
-    name                  { Faker::Name.name }
-    email                 "mmm@gmail.com"
-    password              "00000000"
-    password_confirmation "00000000"
+#    id                    "1"
+    name                  Faker::Name.name
+    email                 Faker::Internet.email
+    password              pass
+    password_confirmation pass
   end
 end
