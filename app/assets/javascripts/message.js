cf880759522a31228e93f1137abbc48db7adec5e
コミットしました <yumayo14@gmail.com>
2017-12-19 17:56:39 +0900

    turbolinksの修正、及びそのた修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,45 +1,47 @@
-$(function() {
-  function buildHTML(message) {
+$(document).on('turbolinks:load', function () {
+  $(function() {
+    function buildHTML(message) {
+      var html = `<div class= chatspace-content>
+                    <div class= chatspace-content__member> ${message.name} </div>
+                    <div class= chatspace-content__post-time> ${message.created_at}</div>
+                    <div class= chatspace-content__chat> ${message.text} </div>
+  `
 
-    var html = `<div class= chatspace-content>
-                  <div class= chatspace-content__member> ${message.name} </div>
-                  <div class= chatspace-content__post-time> ${message.created_at}</div>
-                  <div class= chatspace-content__chat> ${message.text} </div>
-`
+      html +=
+      (message.image.url) ?
+        (
+          `<div class= chatspace-content__chat> <img src= "${message.image.url}"></div> </div>`
+        ):
+        (
+          `</div>`
+        );
+      return html;
+    }
 
-    html =+
-    (message.image.url) ?
-      (
-        `<div class= chatspace-content__chat> <img src= "${message.image.url}"></div> </div>`
-      ):
-      (
-        `</div>`
-      );
-    return html;
-  }
+    $(".new_message").on("submit", function(e) {
+      e.preventDefault();
+      var formData = new FormData(this);
+      var href = $(this).attr('action');
 
-  $(".new_message").on("submit", function(e) {
-    e.preventDefault();
-    var formData = new FormData(this);
-    var href = $(this).attr('action');
-
-    $.ajax ({
-      url: href,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
+      $.ajax ({
+        url: href,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data) {
+        var html = buildHTML(data)
+        $('.chatspace').append(html);
+        $('.form-box__text-field').val('');
+        $('.form-box__picture-set__field').val('');
+        $('.chatspace').animate({scrollTop: $('.chatspace')[0].scrollHeight}, 'slow');
+      })
+      .fail(function() {
+        alert('error');
+      })
+      return false;
     })
-    .done(function(data) {
-      var html = buildHTML(data)
-      $('.chatspace').append(html);
-      $('.form-box__text-field').val('');
-      $('.chatspace').animate({scrollTop: $('.chatspace')[0].scrollHeight}, 'slow');
-    })
-    .fail(function() {
-      alert('error');
-    })
-    return false;
   })
 })
